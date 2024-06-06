49e16cb0bb3f260470a84466097b6ad4241e8af3
DavidTelenko <davidtelenko11@gmail.com>
2024-06-07 01:01:00 +0300

    assignment refactoring

--- include/evaluator.hpp
+++ include/evaluator.hpp
@@ -6,6 +6,7 @@
 #include <numeric>
 #include <optional>
 #include <string_view>
+#include <utility>
 
 #include "lexer.hpp"
 #include "parser.hpp"
@@ -54,27 +55,23 @@ auto parse_num(const Token<It>& token) -> F {
     return val;
 }
 
-template <class F>
-auto eval_unary_fn(auto fn, auto name, std::vector<F>& stack)
-    -> std::optional<F> {
-    ASSERT(stack.size() >= 1,
+template <class F, class Fn, std::size_t... I>
+auto _eval_fn(Fn fn, auto name, std::vector<F>& stack,
+              std::index_sequence<I...>) -> std::optional<F> {
+    ASSERT(stack.size() >= sizeof...(I),
            "Not enough arguments to `" << name << "` function");
-    const auto a = stack[stack.size() - 1];
-    stack.pop_back();
-    stack.push_back(fn(a));
-    return stack.back();
+    const auto result = fn(stack[stack.size() - 1 - I]...);
+    ((std::cout << stack[stack.size() - 1 - I] << " "), ...) << std::endl;
+    for (size_t i = 0; i < sizeof...(I); ++i) {
+        stack.pop_back();
+    }
+    stack.push_back(result);
+    return result;
 }
 
-template <class F>
-auto eval_binary_fn(auto fn, auto name, std::vector<F>& stack)
-    -> std::optional<F> {
-    ASSERT(stack.size() >= 2,
-           "Not enough arguments to `" << name << "` function");
-    const auto a = stack[stack.size() - 1], b = stack[stack.size() - 2];
-    stack.pop_back();
-    stack.pop_back();
-    stack.push_back(fn(a, b));
-    return stack.back();
+template <size_t params, class F>
+auto eval_fn(auto fn, auto name, std::vector<F>& stack) -> std::optional<F> {
+    return _eval_fn<F>(fn, name, stack, std::make_index_sequence<params>{});
 }
 
 template <class It, class F, class Fn>
@@ -82,66 +79,64 @@ auto try_eval_unary_fn(typename Token<It>::Type, str_view<It> name,
                        std::vector<F>& stack, Fn not_found)
     -> std::optional<F> {
     if (name == "sqrt") {
-        return eval_unary_fn([](F a) { return std::sqrt(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::sqrt(a); }, name, stack);
     } else if (name == "cbrt") {
-        return eval_unary_fn([](F a) { return std::cbrt(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::cbrt(a); }, name, stack);
     }
 
     else if (name == "abs") {
-        return eval_unary_fn([](F a) { return std::fabs(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::fabs(a); }, name, stack);
     }
 
     else if (name == "ln") {
-        return eval_unary_fn([](F a) { return std::log(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::log(a); }, name, stack);
     } else if (name == "lg") {
-        return eval_unary_fn([](F a) { return std::log10(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::log10(a); }, name, stack);
     } else if (name == "exp") {
-        return eval_unary_fn([](F a) { return std::exp(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::exp(a); }, name, stack);
     }
 
     else if (name == "ceil") {
-        return eval_unary_fn([](F a) { return std::ceil(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::ceil(a); }, name, stack);
     } else if (name == "floor") {
-        return eval_unary_fn([](F a) { return std::floor(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::floor(a); }, name, stack);
     } else if (name == "round") {
-        return eval_unary_fn([](F a) { return std::round(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::round(a); }, name, stack);
     } else if (name == "trunc") {
-        return eval_unary_fn([](F a) { return std::trunc(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::trunc(a); }, name, stack);
     }
 
     else if (name == "sin") {
-        return eval_unary_fn([](F a) { return std::sin(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::sin(a); }, name, stack);
     } else if (name == "asin") {
-        return eval_unary_fn([](F a) { return std::asin(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::asin(a); }, name, stack);
     } else if (name == "sinh") {
-        return eval_unary_fn([](F a) { return std::sinh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::sinh(a); }, name, stack);
     } else if (name == "asinh") {
-        return eval_unary_fn([](F a) { return std::asinh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::asinh(a); }, name, stack);
     }
 
     else if (name == "cos") {
-        return eval_unary_fn([](F a) { return std::cos(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::cos(a); }, name, stack);
     } else if (name == "acos") {
-        return eval_unary_fn([](F a) { return std::acos(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::acos(a); }, name, stack);
     } else if (name == "cosh") {
-        return eval_unary_fn([](F a) { return std::cosh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::cosh(a); }, name, stack);
     } else if (name == "acosh") {
-        return eval_unary_fn([](F a) { return std::acosh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::acosh(a); }, name, stack);
     }
 
     else if (name == "tan") {
-        return eval_unary_fn([](F a) { return std::tan(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::tan(a); }, name, stack);
     } else if (name == "atan") {
-        return eval_unary_fn([](F a) { return std::atan(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::atan(a); }, name, stack);
     } else if (name == "tanh") {
-        return eval_unary_fn([](F a) { return std::tanh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::tanh(a); }, name, stack);
     } else if (name == "atanh") {
-        return eval_unary_fn([](F a) { return std::atanh(a); }, name, stack);
+        return eval_fn<1>([](F a) { return std::atanh(a); }, name, stack);
     }
 
-    not_found();
-
-    return std::nullopt;
+    return not_found();
 }
 
 template <class It, class F, class Fn>
@@ -150,35 +145,35 @@ auto try_eval_binary_fn(typename Token<It>::Type type, str_view<It> name,
     -> std::optional<F> {
     switch (type) {
         case Token<It>::Add:
-            return eval_binary_fn([](auto a, auto b) { return a + b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a + b; }, name,
+                              stack);
         case Token<It>::Sub:
-            return eval_binary_fn([](auto a, auto b) { return a - b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a - b; }, name,
+                              stack);
         case Token<It>::Mul:
-            return eval_binary_fn([](auto a, auto b) { return a * b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a * b; }, name,
+                              stack);
         case Token<It>::Div:
-            return eval_binary_fn([](auto a, auto b) { return a / b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a / b; }, name,
+                              stack);
         case Token<It>::Mod:
-            return eval_binary_fn(std::fmod<F, F>, name, stack);
+            return eval_fn<2>(std::fmod<F, F>, name, stack);
         case Token<It>::Pow:
-            return eval_binary_fn(std::pow<F, F>, name, stack);
+            return eval_fn<2>(std::pow<F, F>, name, stack);
         case Token<It>::LessThan:
-            return eval_binary_fn([](auto a, auto b) { return a < b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a < b; }, name,
+                              stack);
         case Token<It>::LessEquals:
-            return eval_binary_fn([](auto a, auto b) { return a <= b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a <= b; }, name,
+                              stack);
         case Token<It>::GreaterThan:
-            return eval_binary_fn([](auto a, auto b) { return a > b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a > b; }, name,
+                              stack);
         case Token<It>::GreaterEquals:
-            return eval_binary_fn([](auto a, auto b) { return a >= b; }, name,
-                                  stack);
+            return eval_fn<2>([](auto a, auto b) { return a >= b; }, name,
+                              stack);
         case Token<It>::Equals:
-            return eval_binary_fn(
+            return eval_fn<2>(
                 [](auto a, auto b) { return std::abs(b - a) <= 1e-40; }, name,
                 stack);
         default:
@@ -186,76 +181,66 @@ auto try_eval_binary_fn(typename Token<It>::Type type, str_view<It> name,
     }
 
     if (name == "min") {
-        return eval_binary_fn(std::min<F>, name, stack);
+        return eval_fn<2>(std::min<F>, name, stack);
     } else if (name == "max") {
-        return eval_binary_fn(std::max<F>, name, stack);
+        return eval_fn<2>(std::max<F>, name, stack);
     } else if (name == "log") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) { return std::log(a) / std::log(b); }, name,
             stack);
     } else if (name == "gcd") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) {
                 return std::gcd(static_cast<int>(b), static_cast<int>(a));
             },
             name, stack);
     } else if (name == "lcm") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) {
                 return std::lcm(static_cast<int>(b), static_cast<int>(a));
             },
             name, stack);
     } else if (name == "or") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) {
                 return static_cast<int>(b) or static_cast<int>(a);
             },
             name, stack);
     } else if (name == "and") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) {
                 return static_cast<int>(b) and static_cast<int>(a);
             },
             name, stack);
     } else if (name == "xor") {
-        return eval_binary_fn(
+        return eval_fn<2>(
             [](auto a, auto b) {
                 return static_cast<int>(b) xor static_cast<int>(a);
             },
             name, stack);
     } else if (name == "add") {
-        return eval_binary_fn([](auto a, auto b) { return a + b; }, name,
-                              stack);
+        return eval_fn<2>([](auto a, auto b) { return a + b; }, name, stack);
     } else if (name == "sub") {
-        return eval_binary_fn([](auto a, auto b) { return a - b; }, name,
-                              stack);
+        return eval_fn<2>([](auto a, auto b) { return a - b; }, name, stack);
     } else if (name == "div") {
-        return eval_binary_fn([](auto a, auto b) { return a / b; }, name,
-                              stack);
+        return eval_fn<2>([](auto a, auto b) { return a / b; }, name, stack);
     } else if (name == "mul") {
-        return eval_binary_fn([](auto a, auto b) { return a * b; }, name,
-                              stack);
+        return eval_fn<2>([](auto a, auto b) { return a * b; }, name, stack);
     } else if (name == "mod") {
-        return eval_binary_fn(std::fmod<F, F>, name, stack);
+        return eval_fn<2>(std::fmod<F, F>, name, stack);
     } else if (name == "pow") {
-        return eval_binary_fn(std::pow<F, F>, name, stack);
+        return eval_fn<2>(std::pow<F, F>, name, stack);
     }
 
-    not_found();
-
-    return std::nullopt;
+    return not_found();
 }
 
 template <class It, class F, class Fn>
 auto try_eval_fn(typename Token<It>::Type type, str_view<It> name,
-                 std::vector<F>& stack, Fn not_found) {
-    if (not try_eval_binary_fn<It>(type, name, stack, not_found)) {
-        return;
-    }
-
-    if (not try_eval_unary_fn<It>(type, name, stack, not_found)) {
-        return;
-    }
+                 std::vector<F>& stack, Fn not_found) -> std::optional<F> {
+    return try_eval_unary_fn<It>(type, name, stack, [&]() {
+        return try_eval_binary_fn<It>(type, name, stack, not_found);
+    });
 }
 
 template <class F, class It>
@@ -280,17 +265,21 @@ constexpr auto perform(const Token<It>& identifier, std::vector<F>& stack,
 
     if (identifier.type == Token<It>::Assign) {
         ASSERT(not variables_stack.empty(), "Invalid assignment target");
-
         vars.set(variables_stack.back(), stack.back());
-
         variables_stack.pop_back();
         return true;
     }
 
-    try_eval_fn<It>(identifier.type, identifier_name, stack,
-                    [&variables_stack, &identifier_name]() {
-                        variables_stack.push_back(identifier_name);
-                    });
+    if (not try_eval_fn<It>(identifier.type, identifier_name, stack,
+                            [&variables_stack, &identifier_name]() {
+                                std::cout
+                                    << "Unknown identifier: " << identifier_name
+                                    << '\n';
+                                variables_stack.push_back(identifier_name);
+                                return std::make_optional(0);
+                            })) {
+        return std::nullopt;
+    }
 
     return true;
 }
@@ -319,6 +308,8 @@ constexpr auto evaluate(It begin, It end,
             return std::nullopt;
         }
     }
+
+    calc::print([](auto&& v) { std::cout << v; }, stack)('\n');
 
     ASSERT(stack.size() == 1, "Reduntant values");
 
